4631b3289814a42ef647b973f7a814dee0e59aed
Kevin Johansmeyer <kevinjohansmeyer@gmail.com>
2021-08-12 12:41:36 -0400

    3 working sliders, plot labels, need to fix A

--- render.js
+++ render.js
@@ -1,16 +1,16 @@
-function message() {
- window.alert("Headphones are recommended for the best user experience. Cellphone and laptop may not be able to produce frequencies near or below 90 Hz.");
-}
+//window.alert("Headphones are recommended for the best user experience. Cellphone and laptop may not be able to produce frequencies near or below 90 Hz.");
 
 'use strict'
 
-// Update function 
+//=============================================================================//
+// ----------------------------- Update function ----------------------------- //
+//=============================================================================//
+
 function updateFunction(alpha, m1sliderval, m2sliderval) {
-    console.log({alpha},{m1sliderval},{m2sliderval});
-    //defining variables
+    // ----------------------------- Defining Variables ----------------------------- //
     const Msun = 0.00000492686088; //mass of the sun using geometric units to get f in Hz
 
-    let A = 3.2, //change later to exact calculation
+    let A = 1.343797621, //change later to exact calculation
         deltat = 0.00001,
         tmax = 3,
         N = tmax / deltat,
@@ -22,110 +22,65 @@ function updateFunction(alpha, m1sliderval, m2sliderval) {
 
     t[0] = 0;
     for (let i = 1; i < N; i++) {
-        t[i] = t[i-1] + deltat;
+        t[i] = t[i - 1] + deltat;
     }
 
-    //these need to be changed to work with sliders
-    let m1 = m1sliderval*Msun,
-        m2 = m2sliderval*Msun,
-        M = (m1+m2);
+    let m1 = m1sliderval * Msun,
+        m2 = m2sliderval * Msun,
+        M = (m1 + m2);
 
     //initial conditions:
     f[0] = 90;
-    //v[0] = (Math.PI*M*f[0])**(1/3);
-    v[0] = Math.pow(Math.PI*M*f[0],1/3);
+    v[0] = Math.pow(Math.PI * M * f[0], 1 / 3);
 
-    console.log(f[0], v[0]);
 
-    let eta = (m1*m2)/(M*M), //reduced mass ratio, varies from 0 to 0.25
+    // ----------------------------- Calculations ----------------------------- //
+    let eta = (m1 * m2) / (M * M), //reduced mass ratio, varies from 0 to 0.25
         phic = 0,
-        //tc = t[0]+(5/256)*(M/(eta*v[0]**8));
-        tc = t[0]+(5/256)*(M/(eta*Math.pow(v[0],8)));
+        tc = t[0] + (5 / 256) * (M / (eta * Math.pow(v[0], 8)));
 
-    for (let n = 0; n < N; n++) { //commented code is previous Python
-        //v[n] = ((256/5)*(eta/M)*(tc-t[n]))**(-1/8); //Solution to dv/dt = 32/5 (eta/M) v^9
-        v[n] = Math.pow((256/5)*(eta/M)*(tc-t[n]),-1/8);
-        //phi[n] = phic-(1/5)*(5/eta)**(3/8)*((tc-t[n])/M)**(5/8); //Solution to dphi/dt = v^3/M
-        phi[n] = phic-(1/5)*Math.pow(5/eta,3/8)*Math.pow((tc-t[n])/M,5/8); //Solution to dphi/dt = v^3/M
-        //h[n] = A*((v[n]**2)*math.sin(2*phi[n])+alpha*(v[n]**3)*math.sin(3*phi[n]))
-        h[n] = A*((Math.pow(v[n],2))*Math.sin(2*phi[n])+alpha*(Math.pow(v[n],3)*Math.sin(3*phi[n])));
-        //f[n] = v[n]**3/(Math.pi*M)
-        f[n] = Math.pow(v[n],3)/(Math.PI*M);
+    for (let n = 0; n < N; n++) {
+        v[n] = Math.pow((256 / 5) * (eta / M) * (tc - t[n]), -1 / 8); //Solution to dv/dt = 32/5 (eta/M) v^9
+        phi[n] = phic - (1 / 5) * Math.pow(5 / eta, 3 / 8) * Math.pow((tc - t[n]) / M, 5 / 8); //Solution to dphi/dt = v^3/M
+
+        h[n] = A * ((Math.pow(v[n], 2)) * Math.sin(2 * phi[n]) + alpha * (Math.pow(v[n], 3) * Math.sin(3 * phi[n])));
+        f[n] = Math.pow(v[n], 3) / (Math.PI * M);
     }
-    console.log(t, v ,phi ,h ,f);
+    console.log({ v }, { phi }, { h }, { f });
 
-    // Move to CSS?
-    // let layout = {
-
-    //     title {
-    //         text: 'Plot Title',
-    //         size: 24
-    //     },
-    // };
-    let layout0 ={
+    // ----------------------------- Plotting ----------------------------- //
+    // Strain vs. Time Plot
+    let layout0 = {
         title: {
             text: 'Strain vs. Time',
             font: {
-            family: 'Times New Roman',
-            size: 24,
-            color: 'white'
+                family: 'Times New Roman',
+                size: 24,
+                color: 'white'
             },
         },
         xaxis: {
             title: {
-              text: 'Time',
-              font: {
-                family: 'Courier New, monospace',
-                size: 18,
-                color: 'white'
+                text: 'Time',
+                font: {
+                    family: 'Courier New, monospace',
+                    size: 18,
+                    color: 'white'
                 }
             }
         },
         yaxis: {
             title: {
-              text: 'Strain',
-              font: {
-                family: 'Courier New, monospace',
-                size: 18,
-                color: 'white'
+                text: 'Strain',
+                font: {
+                    family: 'Courier New, monospace',
+                    size: 18,
+                    color: 'white'
                 }
             }
         },
-        plot_bgcolor:"#383838",
-        paper_bgcolor:"#181818"
-    }
-
-    let layout1 ={
-        title: {
-            text: 'Frequency vs. Time',
-            font: {
-            family: 'Times New Roman',
-            size: 24,
-            color: 'white'
-            },
-        },
-        xaxis: {
-            title: {
-              text: 'Time',
-              font: {
-                family: 'Courier New, monospace',
-                size: 18,
-                color: 'white'
-                }
-            }
-        },
-        yaxis: {
-            title: {
-              text: 'Frequency',
-              font: {
-                family: 'Courier New, monospace',
-                size: 18,
-                color: 'white'
-                }
-            }
-        },
-        plot_bgcolor:"#383838",
-        paper_bgcolor:"#181818"
+        plot_bgcolor: "#383838",
+        paper_bgcolor: "#181818"
     }
 
     let trace0 = {
@@ -137,7 +92,41 @@ function updateFunction(alpha, m1sliderval, m2sliderval) {
 
     let data0 = [trace0];
 
-    Plotly.newPlot('myDiv0',data0, layout0);
+    Plotly.newPlot('strainVsTimePlot', data0, layout0);
+    
+    // Frequency vs. Time Plot
+    let layout1 = {
+        title: {
+            text: 'Frequency vs. Time',
+            font: {
+                family: 'Times New Roman',
+                size: 24,
+                color: 'white'
+            },
+        },
+        xaxis: {
+            title: {
+                text: 'Time',
+                font: {
+                    family: 'Courier New, monospace',
+                    size: 18,
+                    color: 'white'
+                }
+            }
+        },
+        yaxis: {
+            title: {
+                text: 'Frequency',
+                font: {
+                    family: 'Courier New, monospace',
+                    size: 18,
+                    color: 'white'
+                }
+            }
+        },
+        plot_bgcolor: "#383838",
+        paper_bgcolor: "#181818"
+    }
 
     let trace1 = {
         x: t,
@@ -146,10 +135,10 @@ function updateFunction(alpha, m1sliderval, m2sliderval) {
     };
 
     let data1 = [trace1];
-    Plotly.newPlot('myDiv1', data1, layout1);
+    Plotly.newPlot('frequencyVsTimePlot', data1, layout1);
 }
 
-//UI Elements
+// ----------------------------- UI Elements ----------------------------- //
 const alphaSlider = document.getElementById("alphaSlider");
 const m1slider = document.getElementById("m1slider");
 const m2slider = document.getElementById("m2slider");
@@ -158,33 +147,31 @@ let alpha = Number(alphaSlider.value),
     m1sliderval = Number(m1slider.value),
     m2sliderval = Number(m2slider.value);
 
-console.log({alpha},{m1sliderval},{m2sliderval});
+console.log({ alpha }, { m1sliderval }, { m2sliderval });
 
-// DEBUG:
+// ----------------------------- Debug ----------------------------- //
 function printVars() {
-    console.log({alpha},{m1sliderval},{m2sliderval});
+    console.log({ alpha }, { m1sliderval }, { m2sliderval });
 }
 
-alphaSlider.addEventListener('change', function(event) {
+alphaSlider.addEventListener('change', function (event) {
     alpha = Number(alphaSlider.value);
     printVars();
     updateFunction(alpha, m1sliderval, m2sliderval);
     //keep zoom window?
-    
+
 })
 
-m1slider.addEventListener('change', function(event) {
+m1slider.addEventListener('change', function (event) {
     m1sliderval = Number(m1slider.value);
     printVars();
     updateFunction(alpha, m1sliderval, m2sliderval);
 })
 
-m2slider.addEventListener('change', function(event) {
+m2slider.addEventListener('change', function (event) {
     m2sliderval = Number(m2slider.value);
     printVars();
     updateFunction(alpha, m1sliderval, m2sliderval);
 })
 
 updateFunction(alpha, m1sliderval, m2sliderval);
-
-//message();
